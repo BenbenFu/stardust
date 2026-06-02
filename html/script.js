8524bf9b2fb87e393d4b2c54a5ac7f9767525f79
BenbenFu <42853316+BenbenFu@users.noreply.github.com>
2026-06-02 17:05:09 +0800

    Update script.js

--- html/script.js
+++ html/script.js
@@ -31,9 +31,14 @@ async function logout() {
 
 // 渲染难度星级
 function renderDifficultyStars(difficulty) {
+    let numStars = parseInt(difficulty);
+    if (isNaN(numStars)) {
+        const starMatch = String(difficulty).match(/(\u2605|\u2B50)/g);
+        numStars = starMatch ? starMatch.length : 0;
+    }
     let stars = '';
     for (let i = 1; i <= 5; i++) {
-        if (i <= difficulty) {
+        if (i <= numStars) {
             stars += '<svg width="24" height="24" viewBox="0 0 12 12" style="display:inline;vertical-align:middle;"><path d="M6 1L7 5 11 5 8 8 9 12 6 9 3 12 4 8 1 5 5 5Z" fill="#1e2622"/></svg>';
         } else {
             stars += '<svg width="24" height="24" viewBox="0 0 12 12" style="display:inline;vertical-align:middle;"><path d="M6 1L7 5 11 5 8 8 9 12 6 9 3 12 4 8 1 5 5 5Z" fill="none" stroke="#1e2622" stroke-width="1"/></svg>';
